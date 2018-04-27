1fa219393c825ccc621f43c8c0e17e19cfdcd01c
Timmy Jing <timmyjing@gmail.com>
2018-04-27 13:09:58 -0700

    Initial commit

---
+++ Hash_map/lib/p01_int_set.rb
@@ -0,0 +1,88 @@
+class MaxIntSet
+  def initialize(max)
+    @max = max
+    @store = Array.new(max + 1, false)
+  end
+
+  def insert(num)
+    raise "Out of bounds" if num > @max || num < 0
+    @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    @store[num % num_buckets] << num
+  end
+
+  def remove(num)
+    @store[num % num_buckets].delete(num)
+    nil
+  end
+
+  def include?(num)
+    @store[num % num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
